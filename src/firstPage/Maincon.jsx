9881c01391d7b46b688f17c4c628a9fc91ad4e51
Oladee <deemajor230600@gmail.com>
2023-04-21 23:13:33 +0100

    event listeners to form

--- src/firstPage/Maincon.jsx
+++ src/firstPage/Maincon.jsx
@@ -1,12 +1,44 @@
-
-export default function Maincon(){
-    return(<>
-            <div>
-                <div className="rent">
-                    <h1>Rent a <span>Place</span>  away from <span>Home</span>  in the <span>Metaverse</span> </h1>
-                    <p>we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone </p>
-
-                </div>
-            </div>
-    </>)
+import { useState } from "react";
+export default function Maincon() {
+  return (
+    <>
+      <div>
+        <div className="rent">
+          <h1>
+            Rent a <span>Place</span> away from <span>Home</span> in the
+            <span>Metaverse</span>
+          </h1>
+          <p>
+            we provide you access to luxury and affordable houses in the
+            metaverse, get a chance to turn your imagination to reality at your
+            comfort zone
+          </p>
+          <div>
+            <Form />
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+export function Form() {
+  const [formData,setFormData] = useState("")
+  const [value, setValue] = useState(null);
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+     setFormData(value)}
+  const handleClick = () => setValue("");
+  const changes = (e) => {
+    e.preventDefault();
+    setValue(e.target.value);
+  };
+  return (
+    <>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" name="" id="searchBar" />
+        <button onClick={handleClick} className="search">Search</button>
+      </form>
+    </>
+  );
 }
