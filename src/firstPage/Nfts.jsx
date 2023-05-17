3ed66b9eef51064640fec3627334cbb6d462ad2d
oladee <deemajor230600@gmail.com>
2023-05-17 13:09:07 +0100

    done with nft write up

--- src/firstPage/Nfts.jsx
+++ src/firstPage/Nfts.jsx
@@ -5,7 +5,7 @@ export default function Nfts(){
         <div className="nfts-wirteup">
             <h3>Metabnb NFTs</h3>
             <p>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</p>
-            <Button>Learn More</Button>
+            <Button className="learn-more">Learn More</Button>
         </div>
         </section>
     </>)
